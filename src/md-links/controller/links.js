2b639f961934c5332abc62e0615aa3fdb7343272
LeslieZarate <fiorelly30zarate@gmail.com>
2019-06-04 19:25:54 -0500

    funcion de obtener links

--- src/md-links/controller/links.js
+++ src/md-links/controller/links.js
@@ -1,16 +1,17 @@
 const fs = require('fs');
+const path = require('path')
 
 export const checkRouteIsFile = (route)  =>{
-  return fs.statSync(route).isFile()
+  return fs.statSync(route, (err,data)=>{
+    if(err)
+
+  }).isFile()
 }
+,res=>conso
 
 export const checkRouteIsDirectory = (route)  =>{
   return fs.statSync(route).isDirectory()
 }
-
-
-
-
 
 /*
 LINUX
@@ -22,3 +23,42 @@ console.log(checkRouteIsFile("E:/LABORATORIA/LIM009-fe-md-links/src/md-links"))
 
 console.log(checkRouteIsDirectory("E:/LABORATORIA/LIM009-fe-md-links/src/md-links/index.js"))
 console.log(checkRouteIsDirectory("E:/LABORATORIA/LIM009-fe-md-links/src/md-links/"))*/
+
+/*
+const getPathsOfDirectory = (route)=>{
+  let routesDirectory = []; 
+  if(fs.statSync(route).isFile()){    
+    routesDirectory.push(route)
+  }  
+  else{
+    fs.readdirSync(route).forEach(paths =>{     
+      let file = path.join(route, paths);
+      routesDirectory = routesDirectory.concat(getPathsOfDirectory(file))
+    });
+  } 
+  return routesDirectory
+}
+*/
+
+
+const getPathsOfDirectory = (route)=>{  
+  let routesDirectory = [];
+  fs.readdirSync(route).forEach(paths =>{     
+    const file = path.join(route, paths);
+    if(fs.statSync(file).isFile()){
+      routesDirectory.push(file)
+    }else{
+      routesDirectory = routesDirectory.concat(getPathsOfDirectory(file))    
+    }    
+  })
+
+  return routesDirectory
+}
+
+
+
+console.log(getPathsOfDirectory ('/home/leslie/Documents/LIM009-fe-md-links/'))
+
+
+
+
